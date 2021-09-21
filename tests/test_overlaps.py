0bb26f6f11fca4ea3566cb17bd0ad6da8336309b
Alexey Siretskiy <alexey.siretskiy@gmail.com>
2021-09-21 10:07:22 +0200

    added tests for overlaps

---
+++ tests/test_overlaps.py
@@ -0,0 +1,34 @@
+"""
+    testing for the overlap module
+"""
+
+# import pytest
+
+try:
+    from sim import overlaps
+except ModuleNotFoundError:
+    from  sim.sim import   overlaps
+
+
+def test_n_conf():
+    """
+    testing number of conformations for a given `N`
+    :return: True/False
+    :rtype: bool
+    """
+
+    overlaps_10 = overlaps.Overlap(N=10)
+
+    assert  overlaps_10.n_conformations == 1172556
+
+
+def test_overlap_distribution():
+    """
+    checking the distribution for overlaps
+    :return: True/False
+    :rtype: bool
+    """
+
+    overlaps_4 = overlaps.Overlap(N=4)
+
+    assert  overlaps_4.get_overlaps_histogram() == {0: 24, 1: 60, 2: 6}
