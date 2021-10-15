121d34206b7bf3035beafd01536c3eee3c759eda
Alexey Siretskiy <alexey.siretskiy@gmail.com>
2021-10-15 11:10:32 +0200

    Big change
    added functions prepare_simulation and run_simulation;
    fixed import for Jupyter notebook visualization

--- sim/sim.py
+++ sim/sim.py
@@ -12,31 +12,34 @@ import matplotlib.pyplot as plt
 
 import random
 
+
 try:
-    from sim.consts import N
+    # from sim.consts import N
     from sim.cell import CubicCell
     # from sim.aux import *
     from sim.moves import Kink, CrankShaft, Pin
     from sim.polymer import Polymer
 
 except ModuleNotFoundError:
-    from cell import CubicCell
+    try:
+        from cell import CubicCell
+        # from consts import  N
+        # from aux import *
+        from moves import Kink,CrankShaft, Pin
+        from polymer import Polymer
+    except:
+        from .cell import  CubicCell
+        from .moves import  Kink, CrankShaft,Pin
+        from .polymer import Polymer
 
-    from consts import  N
-    # from aux import *
-    from moves import Kink,CrankShaft, Pin
-    from polymer import Polymer
 
-
-
-
-def run_simulation():
+def prepare_simulation(a,b,c, n):
     """
-    running the  simulation
+    prepare simulation
     :return:
     :rtype:
     """
-    cell = CubicCell(6, 10, 6)
+    cell = CubicCell(a, b, c)
     print(cell)
     print(cell.get_center())
 
@@ -44,11 +47,10 @@ def run_simulation():
     print(kink)
     crankshaft = CrankShaft()
     print(crankshaft)
-
     pin = Pin()
     print(pin)
 
-    polymer = Polymer(N, cell, kink, crankshaft, pin)
+    polymer = Polymer(n, cell, kink, crankshaft, pin)
     print(polymer)
     print('squashed coordinates are:')
     print(polymer.get_coords())
@@ -56,23 +58,44 @@ def run_simulation():
     print('unrolled coordinates are:')
     print(polymer.unroll_chain())
 
+    return polymer
 
-    for step in range(1000):
+
+
+def run_simulation(polymer, scatter, lines, show=False, n_steps = 1000) :
+    """
+    running the  simulation
+    :return:
+    :rtype:
+    """
+
+
+    for step in range(n_steps):
         coords_save = polymer.coords.copy()
         rnd = random.random()
         if rnd < 0.4:
-            crankshaft.coordinates = polymer.coords
+            polymer.move_crankshaft.coordinates = polymer.coords
+            # crankshaft.coordinates = polymer.coords
             polymer.coords_tmp = polymer.move_crankshaft.getOutput(1)
         elif (rnd > 0.4) & (rnd < 0.7):
-            kink.coordinates = polymer.coords  #
+            polymer.move_kink.coordinates =  polymer.coords
+            # kink.coordinates = polymer.coords  #
             polymer.coords_tmp = polymer.move_kink.getOutput(1)
         else:
-            #         print('pin')
-            pin.coordinates = polymer.coords
+            # print('pin')
+            polymer.move_pin.coordinates  = polymer.coords
+            # pin.coordinates = polymer.coords
             polymer.coords_tmp = polymer.move_pin.getOutput(1)
 
         if polymer.check_borders() & polymer.check_overlap():
             polymer.coords = polymer.coords_tmp.copy()
+            if show:
+                scatter.x = polymer.coords[0, :];
+                scatter.y = polymer.coords[1, :];
+                scatter.z = polymer.coords[2, :];
+                lines.x = polymer.coords[0, :];
+                lines.y = polymer.coords[1, :];
+                lines.z = polymer.coords[2, :];
 
         else:
             polymer.coords = coords_save
@@ -84,4 +107,5 @@ def run_simulation():
 
 if __name__ == "__main__":
 
-    run_simulation()
+    polymer =  prepare_simulation(10,10,10, 50)
+    run_simulation(polymer)
