a11a9fb82ca941d39cee142c3bb002736f3256cc
Alexey Siretskiy <alexey.siretskiy@gmail.com>
2021-11-04 13:52:56 +0100

    added two polymers
    added two polymers and new prepare_sim and run_sim functions.
    Fix in acceptance criteria.

--- sim/sim.py
+++ sim/sim.py
@@ -108,10 +108,15 @@ def run_simulation(polymer, scatter=None, lines=None, ori_ter=None, show=False, 
                 ind2 = random.randint(0, polymer.coords.shape[1]-1)
 
             # print('before ', a,b, abs(ind2-ind1), ind1, ind2)
-            polymer.coords_tmp = polymer.move_rosenbluth.getOutput(ind1, ind2, cached_counts, small_ark=False)
+            rnd = random.random()
+            if rnd > .5:
+                ori_ark = True
+            else:
+                ori_ark = False
+            polymer.coords_tmp = polymer.move_rosenbluth.getOutput(ind1, ind2, cached_counts, ori_ark=ori_ark)
         # print('diff\n ', repr(polymer.coords),  repr(polymer.coords_tmp))
 
-        rnd = random.random()
+        # rnd = random.random()
 
         # if rnd < 0.4:
         #     # if hasattr(polymer, 'move_crankshaft'):
@@ -150,7 +155,7 @@ def run_simulation(polymer, scatter=None, lines=None, ori_ter=None, show=False, 
             if (energy_new-energy_old) < rnd:
                 polymer.coords = polymer.coords_tmp.copy()
                 # polymer.coords = np.roll(polymer.coords, random.randint(1, polymer.coords.shape[1]), axis=1)
-                if show & (step%2 == 0) :
+                if show & (step%100 == 0) :
                     # print('show')
                     scatter.x = polymer.coords[0, :];
                     scatter.y = polymer.coords[1, :];
@@ -175,6 +180,162 @@ def run_simulation(polymer, scatter=None, lines=None, ori_ter=None, show=False, 
     print(polymer.coords)
     return walks, distances
 
+
+def run_simulation_2(polymer1, polymer2, scatter1=None, lines1=None, scatter2=None, lines2=None, ori_ter=None, show=False, n_steps=1000,
+                     use_moves=['move_rosenbluth']):
+
+
+    """
+    running the  simulation
+    :return:
+    :rtype:
+    """
+    print('caching  counts...')
+    cached_counts = cache_n_conf(polymer1.n, polymer1.cell.A, polymer1.cell.B, polymer1.cell.C)
+    print('done')
+
+    walks = []
+    distances = []
+    for step in range(n_steps):
+        coords_save = polymer1.coords.copy()
+        if hasattr(polymer1, 'move_rosenbluth') and 'move_rosenbluth' in use_moves:
+            polymer1.move_rosenbluth.coordinates = polymer1.coords.copy()
+
+            ind1 = random.randint(0, polymer1.coords.shape[1] - 1)
+            ind2 = ind1
+            while ind2 == ind1:
+                # print('stuck')
+                ind2 = random.randint(0, polymer1.coords.shape[1] - 1)
+
+            # print('before ', a,b, abs(ind2-ind1), ind1, ind2)
+            rnd = random.random()
+            if rnd > .5:
+                ori_ark = True
+            else:
+                ori_ark = False
+            polymer1.coords_tmp = polymer1.move_rosenbluth.getOutput(ind1, ind2, cached_counts, ori_ark=ori_ark)
+            polymer2.move_rosenbluth.coordinates = polymer1.coords_tmp.copy()
+
+            polymer2.coords_tmp = polymer2.move_rosenbluth.getOutput(0,39, cached_counts, ori_ark=False)
+
+        # print('diff\n ', repr(polymer1.coords),  repr(polymer1.coords_tmp))
+
+
+        if polymer1.check_borders() &  polymer2.check_borders(): # & polymer1.check_overlap() :
+            # calculatin energy for the new configuration
+            t = np.mean(polymer1.coords_tmp, axis=1)
+            dst_new1 = polymer1.cell.f_f.get_distance(t[1])
+            energy_new1 = polymer1.cell.f_f.get_value()(dst_new1)
+
+            t = np.mean(polymer2.coords_tmp, axis=1)
+            dst_new2 =  polymer2.cell.B  - polymer2.cell.f_f.get_distance(t[1])
+            energy_new2 = polymer2.cell.f_f.get_value()(dst_new2)
+
+            energy_new = energy_new2 + energy_new1
+
+            # calculatin energy for the old configuration
+            t = np.mean(polymer1.coords, axis=1)
+            dst_old1 = polymer1.cell.f_f.get_distance(t[1])
+            energy_old1 = polymer1.cell.f_f.get_value()(dst_old1)
+
+            t = np.mean(polymer2.coords, axis=1)
+            dst_old2 = polymer2.cell.B  - polymer2.cell.f_f.get_distance(t[1])
+
+            # print(dst_old2, t[1])
+            energy_old2 = polymer2.cell.f_f.get_value()(dst_old2)
+            energy_old = energy_old1 + energy_old2
+
+            # print(energy_old, energy_new)
+            if (np.exp(-energy_new + energy_old)) >= random.random():
+                polymer1.coords = polymer1.coords_tmp.copy()
+                polymer2.coords = polymer2.coords_tmp.copy()
+
+                # polymer1.coords = np.roll(polymer1.coords, random.randint(1, polymer1.coords.shape[1]), axis=1)
+                if show & (step % 100 == 0):
+                    # print('show')
+                    scatter1.x = polymer1.coords[0, :];
+                    scatter1.y = polymer1.coords[1, :];
+                    scatter1.z = polymer1.coords[2, :];
+                    lines1.x = polymer1.coords[0, :];
+                    lines1.y = polymer1.coords[1, :];
+                    lines1.z = polymer1.coords[2, :];
+
+                    scatter2.x = polymer2.coords[0, :];
+                    scatter2.y = polymer2.coords[1, :];
+                    scatter2.z = polymer2.coords[2, :];
+                    lines2.x = polymer2.coords[0, :];
+                    lines2.y = polymer2.coords[1, :];
+                    lines2.z = polymer2.coords[2, :];
+
+                    ori_ter.x = polymer1.coords[0, [0, polymer1.n // 2]]
+                    ori_ter.y = polymer1.coords[1, [0, polymer1.n // 2]]
+                    ori_ter.z = polymer1.coords[2, [0, polymer1.n // 2]]
+
+                    t = np.mean(polymer1.coords, axis=1)
+                    # print("%.1f, %.1f, %.1f"%(t[0], t[1], t[2]))
+                    dst = polymer1.cell.f_f.get_distance(t[1])
+                    walks.append(t)
+                    distances.append(dst)
+        else:
+            # print('outside')
+            polymer1.coords = coords_save
+
+    print("after  moves")
+    print(polymer1.coords)
+    return walks, distances
+
+
+
+def prepare_simulation_2(a,b,c, n):
+    """
+    prepare simulation
+    :return:
+    :rtype:
+    """
+
+    f_f = ForceField(linear=True, amplitude=17)
+    f_f.origin = b
+    print(f_f)
+
+
+    # cell = CubicCell(a, b, c)
+    cell = ForceCubicCell(a,b,c, f_f)
+    print(cell)
+    print(cell.get_center())
+    print("the force origin is at: %s" %cell.f_f.origin)
+
+
+    kink = Kink()
+    print(kink)
+    crankshaft = CrankShaft()
+    print(crankshaft)
+    pin = Pin()
+    print(pin)
+
+    rosenbluth = Rosenbluth()
+    print(rosenbluth)
+
+
+    polymer1 = Polymer(n, cell, kink, crankshaft, pin, rosenbluth)
+    polymer2 = Polymer(n, cell, kink, crankshaft, pin, rosenbluth)
+
+    # polymer = Polymer(n, cell, rosen)
+
+    print(polymer1)
+    print('squashed coordinates are:')
+    print(polymer1.get_coords())
+
+    print('unrolled coordinates are:')
+    print(polymer1.unroll_chain())
+
+    print(polymer2)
+    print('squashed coordinates are:')
+    print(polymer2.get_coords())
+
+    print('unrolled coordinates are:')
+    print(polymer2.unroll_chain())
+
+    return polymer1, polymer2
 
 
 if __name__ == "__main__":
